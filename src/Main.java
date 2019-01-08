103fa9056165aac87e9bd957e1389dc2ede9f2f0
maxfrancis1 <max.francis2000@gmail.com>
2019-01-08 09:26:32 +0000

    Added shop
    Added shop where you can purchase a syringe or beer.

--- src/Main.java
+++ src/Main.java
@@ -18,9 +18,9 @@ public class Main {
         System.out.println("Welcome to the city of Valentine\n");
 
         GAME:
-        while(running){
+        while(running) {
 
-            if(health <= 0){
+            if (health <= 0) {
                 System.out.println("\n\t YOU ARE DEAD");
                 break;
             }
@@ -28,18 +28,42 @@ public class Main {
             System.out.println("\t# Your health is at " + health);
             System.out.println("\t# You have " + money + " gold");
             System.out.println("\t\nWhat would you like to do?");
-            System.out.println("\t1. Rob bank");
-            System.out.println("\t2. Rob someone\n");
+            System.out.println("\t1. Browse the shop");
+            System.out.println("\t2. Rob bank");
+            System.out.println("\t3. Rob someone\n");
 
             String input = in.nextLine();
 
-            if (input.equals("1")){
+            if (input.equals("1")) {
+
+                System.out.println("\tYou see a bottle of beer and a syringe on the shelf, it looks dirty.\n");
+                System.out.println("\tShopkeeper: Welcome, would you like to purchase?\n");
+
+                System.out.println("\t1. Beer - 5 gold\n\t2. Syringe - 15 gold");
+
+                String inputshop = in.nextLine();
+
+                if(inputshop.equals("1")){
+
+                    System.out.println("\nYou bought then drank the bottle of beer. ( -5 gold and -5 health, you did have some fun though)\n");
+                    money = money - 5;
+                    health = health - 5;
+
+                }else if(inputshop.equals("2")){
+
+                    System.out.println("\nYou bought then injected yourself with the dirty syringe. ( -15 gold and 10 health)\n");
+                    money = money - 15;
+                    health = health + 10;
+
+                } else System.out.println("\t\tinput not recognised please type '1' or '2'\n");1
+
+            }else if (input.equals("2")){
 
                 int outcome = rand.nextInt(10);
 
                 if (outcome >= 6) {
 
-                    System.out.println("\tYou were caught robbing the bank! (You lose 20 health)");
+                    System.out.println("\tYou were caught robbing the bank! (You lose 20 health)\n");
                     health = health - 20;
 
                 }else if (outcome < 6){
@@ -50,13 +74,13 @@ public class Main {
                 }
 
 
-            } else if (input.equals("2")){
+            } else if (input.equals("3")){
 
                 int outcome = rand.nextInt(10);
 
                 if (outcome >= 8) {
 
-                    System.out.println("\tYou were caught robbing the someone! (You lose 10 health)");
+                    System.out.println("\tYou were caught robbing the someone! (You lose 10 health)\n");
                     health = health - 10;
 
                 }else if (outcome < 8){
