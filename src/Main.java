561c8aa52a71762d8a5df84163dc1ebf94a7f907
maxfrancis1 <max.francis2000@gmail.com>
2019-01-08 09:08:04 +0000

    Testing the MVP with some RNG

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +7,7 @@ public class Main {
 
         // System objects
         Scanner in = new Scanner(System.in);
+        Random rand = new Random();
 
         // Player variables
         int health = 100;
@@ -18,7 +20,7 @@ public class Main {
         GAME:
         while(running){
 
-            System.out.println("\t# Your health is at " + health + "%");
+            System.out.println("\t# Your health is at " + health);
             System.out.println("\t# You have " + money + " gold");
             System.out.println("\t\nWhat would you like to do?");
             System.out.println("\t1. Rob bank");
@@ -28,13 +30,37 @@ public class Main {
 
             if (input.equals("1")){
 
-                System.out.println("\nYou robbed the bank! (You gained 60 gold.)\n");
-                money = money + 60;
+                int outcome = rand.nextInt(10);
+
+                if (outcome >= 6) {
+
+                    System.out.println("\tYou were caught robbing the bank! (You lose 20 health)");
+                    health = health - 20;
+
+                }else if (outcome < 6){
+
+                    System.out.println("\nYou robbed the bank! (You gained 60 gold.)\n");
+                    money = money + 60;
+
+                }
+
 
             } else if (input.equals("2")){
 
-                System.out.println("\nYou robbed the someone! (You gained 15 gold.)\n");
-                money = money + 15;
+                int outcome = rand.nextInt(10);
+
+                if (outcome >= 8) {
+
+                    System.out.println("\tYou were caught robbing the someone! (You lose 10 health)");
+                    health = health - 10;
+
+                }else if (outcome < 8){
+
+                    System.out.println("\nYou robbed someone! (You gained 15 gold.)\n");
+                    money = money + 15;
+
+                }
+
 
             } else System.out.println("input not recognised please type '1' or '2'");
 
