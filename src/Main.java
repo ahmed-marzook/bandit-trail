e95d395b67e671aef61d25606eeadaf100ce94c9
maxfrancis1 <max.francis2000@gmail.com>
2019-01-08 12:25:20 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
 
         // System objects
         Scanner in = new Scanner(System.in);
-        Random rand = new Random();
+        Random rand = new Random(); //RANDOM UTIL
 
         // Game variables
-        String[] towns = {"Valentine", "Rhodes", "Lagras"};
+        String[] towns = {"Valentine", "Rhodes", "Lagras"}; //different towns
         int currentTown = 0;
 
         // Shop variables
@@ -26,12 +26,12 @@ public class Main {
 
         while(running) {
 
-            if (health <= 0) {
+            if (health <= 0) { // Checking if you have more than 0 health
                 System.out.println("\n\t YOU ARE DEAD");
                 break;
             }
 
-            if (currentTown >= towns.length) {
+            if (currentTown >= towns.length) { // When you finish the game this appears
 
                 System.out.println("You have finished the game with " + money + " gold. Well done!");
                 break;
@@ -40,7 +40,7 @@ public class Main {
 
             System.out.println("You are currently in " + towns[currentTown]);
 
-            System.out.println("\n\t# Your health is at " + health);
+            System.out.println("\n\t# Your health is at " + health); // General text pop up
             System.out.println("\t# You have " + money + " gold");
             System.out.println("\t\nWhat would you like to do?");
             System.out.println("\t1. Browse the shop");
@@ -49,24 +49,24 @@ public class Main {
             System.out.println("\t4. Browse the stables");
             System.out.println("\t5. Travel to the next town\n");
 
-            String input = in.nextLine();
+            String input = in.nextLine(); //                    GETTING INPUT FROM THE USER
 
-            if (input.equals("1")) {
+            if (input.equals("1")) { // Main screen input 1
 
                 System.out.println("\tYou see a bottle of beer and a syringe on the shelf, it looks dirty.\n");
                 System.out.println("\tShopkeeper: Welcome, would you like to purchase?\n");
 
                 System.out.println("\t# 1. Beer - 5 gold\n\t# 2. Syringe - 15 gold");
 
-                String inputShop = in.nextLine();
+                String inputShop = in.nextLine(); //                    GETTING INPUT FROM THE USER
 
-                if (inputShop.equals("1")){
+                if (inputShop.equals("1")){ // SHOP INPUT 1
 
                     System.out.println("\nYou bought then drank the bottle of beer. ( -5 gold and -5 health, you did have some fun though)\n");
                     money = money - 5;
                     health = health - 5;
 
-                } else if (inputShop.equals("2")){
+                } else if (inputShop.equals("2")){ // SHOP INPUT 2
 
                     if (amountOfSyringes <= 0) {
 
@@ -84,7 +84,7 @@ public class Main {
                 } else System.out.println("\t\tinput not recognised please type '1' or '2'\n");
 
 
-            } else if (input.equals("2")){
+            } else if (input.equals("2")){ // Main screen input 2
 
                 int outcome = rand.nextInt(10);
 
@@ -101,7 +101,7 @@ public class Main {
                 }
 
 
-            } else if (input.equals("3")){
+            } else if (input.equals("3")){ // Main screen input 2
 
                 int outcome = rand.nextInt(10);
 
@@ -118,14 +118,14 @@ public class Main {
                 }
 
 
-            } else if (input.equals("4")){
+            } else if (input.equals("4")){ // Main screen input 2
 
                 System.out.println("\tStablemaster: Welcome sir would you like to buy a horse?\n");
                 System.out.println("\t# 1. Purchase stallion - 50 gold\n\t# 2. Go back\n");
 
-                String inputStable = in.nextLine();
+                String inputStable = in.nextLine(); //                    GETTING INPUT FROM THE USER
 
-                if (inputStable.equals("1")){
+                if (inputStable.equals("1")){ // HORSE STABLES INPUT
 
                     System.out.println("\tYou have purchased a stallion! (You are now able to travel)\n");
                     playerOwnsHorse = true;
@@ -133,7 +133,7 @@ public class Main {
                 }
 
 
-            } else if(input.equals("5")){
+            } else if(input.equals("5")){ // Main screen input 2
 
                 if(playerOwnsHorse) {
 
