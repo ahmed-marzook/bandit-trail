2cda60bb4378012c60a7689544e9a0162199f20e
Ahmed Marzook <m4marzook@gmail.com>
2019-01-08 14:14:52 +0000

    More choices and stuff

--- src/Game.java
+++ src/Game.java
@@ -1,34 +1,98 @@
-//Importing utilities to my class to be able to use it
+
 import java.util.Scanner;
 
 public class Game {
+    String myWeapon = "N/A", playerName;
+    int myHealth = 100, gold = 100, ammo = 0, notoriety = 0, choice;
+    Scanner myScanner = new Scanner(System.in), enterScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //initialized Variables
-        String myWeapon = "N/A", playerName;
-        int myHealth = 100, gold = 100, ammo = 0, notoriety = 0;
-        Scanner myScanner = new Scanner(System.in);
-
-        //Asking for user name using scanner to save String name into variable
-        System.out.println("Please Enter your name: ");
+        Game game = new Game();
+        game.playerSetUp();
+        game.firstTown();
+    }
+    public void playerSetUp() {
+        System.out.println("Please enter your name: ");
         playerName = myScanner.nextLine();
-        System.out.println(playerName);
+        playerStats();
+    }
 
-        //Asking for user fav number
-        int userNum;
-        System.out.println("Please enter a number:");
-        userNum = myScanner.nextInt();
-        System.out.println(userNum);
-
-        //Intro Text
-        System.out.println("You have just been released from jail, with just " + gold + " gold to your name you head into town, what would you like to do?");
-        System.out.println("Your current stats are:");
+    public void playerStats(){
+        System.out.println("\n---------------------------------------------------------------------------------------------------------------------------------\n");
+        System.out.println(playerName + " stats are:");
         System.out.println("Health: " + myHealth);
         System.out.println("Gold: " + gold);
         System.out.println("Weapon: " + myWeapon);
         System.out.println("Ammo: " + ammo);
         System.out.println("Notoriety: " + notoriety + "%");
 
+    }
+
+    public void firstTown(){
+        System.out.println("\n---------------------------------------------------------------------------------------------------------------------------------\n");
+        System.out.println("You have just been released from jail, with just a bit of gold to your name and you head into town.\nWhat would you like to do?\n");
+        System.out.println("1) Go to the Shop");
+        System.out.println("2) Go to another town");
+        System.out.println("3) Attempt to rob someone");
+        System.out.println("4) Show player stats");
+
+        choice = myScanner.nextInt();
+
+        if (choice == 1){
+            shop();
+        }
+        if (choice == 2){
+            System.out.println("You got to another town");
+        }
+        if (choice == 3){
+            robbery();
+        }
+        if (choice == 4) {
+            playerStats();
+            enterScanner.nextLine();
+            firstTown();
+        }
+    }
+
+    public void shop() {
+        System.out.println("\n---------------------------------------------------------------------------------------------------------------------------------\n");
+        System.out.println("Welcome to the shop please select an item you want to buy.");
+        System.out.println("1) Rifle     40G");
+        System.out.println("2) Revolver  10G");
+        System.out.println("3) Ammo       1G");
+        System.out.println("4) Go back to the town");
+
+        choice = myScanner.nextInt();
+
+        if (choice == 1) {
+            gold = gold - 40;
+            myWeapon = "Rifle";
+            System.out.println("Your Weapon is now a Rifle.\n(-40G from player inventory)");
+            enterScanner.nextLine();
+            shop();
+        }
+        if (choice == 2) {
+            gold = gold - 10;
+            myWeapon = "Revolver";
+            System.out.println("Your Weapon is now a Revolver.\n(-10G from player inventory)");
+            enterScanner.nextLine();
+            shop();
+        }
+        if (choice == 3) {
+            System.out.println("How much ammo do you want: ");
+            choice = myScanner.nextInt();
+            ammo = ammo + choice;
+            gold = gold - choice;
+            System.out.println("You now have " + ammo + " ammo\n(-" + choice + "G from player inventory");
+            enterScanner.nextLine();
+            shop();
+        }
+        if (choice == 4) {
+            firstTown();
+        }
+    }
+
+    public void robbery() {
 
     }
 }
