442e28bf83f9c555542cbed4a7802d14687350c1
maxfrancis1 <max.francis2000@gmail.com>
2019-01-09 14:38:19 +0000

    Final refactor
    The game finished

--- src/Player.java
+++ src/Player.java
@@ -2,9 +2,9 @@ public class Player {
 
     boolean playerOwnsHorse = false;
     boolean mask = false;
-    int health = 2000;
-    int money = 5000;
-    int notoriety = -1000;
+    int health = 100;
+    int money = 50;
+    int notoriety = 0;
     int strength = 1;
 
 }
