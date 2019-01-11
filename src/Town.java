d3908dea8248e8fec1c40c026470deafd6e6e80f
maxfrancis1 <max.francis2000@gmail.com>
2019-01-11 15:00:26 +0000

    refactor

--- src/Town.java
+++ src/Town.java
@@ -179,7 +179,7 @@ public class Town extends Player {
 
             if (outcome >= 8) {
 
-                System.out.println("\t-- You were hurt robbing the someone! ( - 20 health) --\n");
+                System.out.println("\t-- You were hurt robbing the someone! ( - 10 health) --\n");
                 health = health - 10;
 
             } else if (outcome < 8) {
